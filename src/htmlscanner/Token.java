eae2ee0afb55d7b7e56dc28d5506a3005a9f4700
dnoseda <damian.noseda@mercadolibre.com>
2011-04-22 15:28:16 -0300

    working with tdd

--- src/htmlscanner/Token.java
+++ src/htmlscanner/Token.java
@@ -24,5 +24,34 @@ public class Token {
 		this.code = code;
 		this.value = value;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 	
 }
--- test/scanner/AutomatonTests.java
+++ test/scanner/AutomatonTests.java
@@ -1,7 +1,13 @@
 package scanner;
 import htmlscanner.Automaton;
+import htmlscanner.Token;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
 import junit.framework.TestCase;
 
@@ -13,6 +19,40 @@ public class AutomatonTests extends TestCase {
 		
 		// retorna lista de errores y lista de tokens reconocidos
 		ScanResult result = au.scan("");
+		assertTrue(result.getTokens().isEmpty());
+		assertTrue(result.getErrors().isEmpty());
+	}
+	
+	public void testIdent() throws Exception {
 		
+		Automaton au = new Automaton();
+		au.init("simpleconf.yaml");
+		
+		// retorna lista de errores y lista de tokens reconocidos
+		ScanResult result = au.scan("a10 bbb c001");
+		List<Token> tokens = Arrays.asList(new Token("ID","a10"), new Token("SEP",null),
+				new Token("ID","bbb"), new Token("SEP",null),
+				new Token("ID","c001"));
+		assertTrue(String.format("tokens esperado: %s\ntokens resultado: %s",tokens,result.tokens),isEqual(result.getTokens(),tokens));
+	}
+
+	public static boolean isEqual(List<Token> tokens, List<Token> tokens2) {
+		Preconditions.checkNotNull(tokens);
+		Preconditions.checkNotNull(tokens2);
+		
+		if(tokens.isEmpty() && tokens2.isEmpty()){
+			return true;
+		}
+		
+		if(tokens.size() != tokens2.size()){
+			return false;
+		}
+		
+		for(int i = 0; i< tokens.size(); i++){
+			if(!(tokens.get(i).equals(tokens2.get(i)))){
+				return false;
+			}
+		}
+		return true;
 	}
 }

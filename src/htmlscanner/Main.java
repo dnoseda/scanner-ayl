6ea5a86c3ca5cf18d15608c2aed7532978827d56
dnoseda <damian.noseda@mercadolibre.com>
2011-04-22 14:57:03 -0300

    working ok with external yaml config

--- src/htmlscanner/Main.java
+++ src/htmlscanner/Main.java
@@ -1,172 +1,112 @@
 package htmlscanner;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.ho.yaml.Yaml;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class Main {
-	
-	static Map<String,Map<String,Object>> semanticActions = Maps.newHashMap();
-	static Map<String,Map<Character,String>> transitions = Maps.newHashMap();
-	static interface Actionable{
-		void doAction(Automaton automaton);
-	}
-	public static void init(){
-		initINITIAL();
-		
-		initINT();
-		initINT_FINAL();
-		
-		
-		initESP();
-		
-		initIDENT();
-		
-		initIDENT_FINAL();
-	}		
-	private static void initIDENT_FINAL() {
-		
-		Map<String,Object> aux = new ImmutableMap.Builder<String,Object>()
-			.put("token_id", "id")
-			.put("have_value", true)
-			.put("reset", true).build();
-		semanticActions.put("ident_final", aux);
-	}
-	private static void initINT_FINAL() {
-		Map<String,Object> aux = new ImmutableMap.Builder<String,Object>()
-				.put("token_id", "int")
-				.put("have_value", true)
-				.put("reset", true).build();
-		semanticActions.put("int_final", aux);
-				
-		new Actionable() {
-			public void doAction(Automaton automaton) {
-				// retornar identificador
-				System.out.println(String.format("==> en pos %d hasta %d, (INT, %s)", automaton.initialPos, automaton.pos-2, automaton.getTempString()));
-				automaton.tokens.add(new Token("INT",automaton.getTempString()));
-				automaton.execEpsilon();
+
+	static Map<String, Map<String, Object>> semanticActions = Maps.newHashMap();
+	static Map<String, Map<Character, String>> transitions = Maps.newHashMap();
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void init() {
+		try {
+			Map<String, Object> config = Yaml.loadType(new File(
+					"simpleconf.yaml"), HashMap.class);
+			semanticActions = (Map<String, Map<String, Object>>) config
+					.get("semanticActions");
+			Map aux= (Map) config
+					.get("transitions");
+			for(Object obj: aux.entrySet()){
+				Entry ent = (Entry)obj;
+				Map<Character,String> value = Maps.newHashMap();
+				for(Object obj0: ((Map)ent.getValue()).entrySet()){
+					Entry ent1 = (Entry) obj0;
+					value.put(String.valueOf(ent1.getKey()).charAt(0), String.valueOf(ent1.getValue()));
+				}
+				transitions.put((String)ent.getKey(), value);
 			}
-		};
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	private static void initIDENT() {
-		Map<Character, String> aux;
-		aux = new ImmutableMap.Builder<Character,String>()
-											.put('a', "ident")
-											.put('b', "ident")
-											.put('c', "ident")
-											.put('0', "ident")
-											.put('1', "ident")
-											.put(' ', "ident_final")
-											.put('.', "ident_final")
-											.put('$', "ident_final").build();
-		transitions.put("ident", aux);
-		Map<String, Object> auxS = new ImmutableMap.Builder<String, Object>().put("save_value",true).build();
-		semanticActions.put("ident",auxS);
-	}
-	private static void initINT() {
-		Map<Character, String> aux;
-		aux = new ImmutableMap.Builder<Character,String>()
-											.put('a', "int_final")
-											.put('b', "int_final")
-											.put('c', "int_final")
-											.put('0', "int")
-											.put('1', "int")
-											.put(' ', "int_final")
-											.put('.', "int_final")
-											.put('$',"int_final").build();
-		transitions.put("int", aux);
-		Map<String, Object> auxS = new ImmutableMap.Builder<String, Object>().put("save_value",true).build();
-		semanticActions.put("int",auxS);
-	}
-	
-	
-	private static void initESP() {
-		Map<String, Object> auxS = new ImmutableMap.Builder<String, Object>()
-							.put("token_id","SEP")
-						    .put("have_value",false)
-						    .put("reset",false)
-							.build();
-		semanticActions.put("esp",auxS);
-		new Actionable() {
-			public void doAction(Automaton automaton) {
-				//entregar separador
-				System.out.println(String.format("==> en pos %d, (SEP, )", automaton.pos-1));
-				automaton.tokens.add(new Token("SEP",null));
-				
-				// ir a inicial
-				automaton.state = "initial";
-			}
-		};
-	}
-	private static void initINITIAL() {
-		Map<Character,String> aux = new ImmutableMap.Builder<Character,String>()
-											.put('a',"ident")
-											.put('b',"ident")
-											.put('c',"ident")
-											.put('0', "int")
-											.put('1', "int")
-											.put(' ',"esp")
-											.put('$', "end").build();
-		transitions.put("initial", aux);
-	}
+
 	static class Automaton {
 		protected int initialPos;
 		int pos = 0;
 		char index;
 		String state = "initial";
 		List<Character> temp = Lists.newArrayList();
-		String getTempString(){
+
+		String getTempString() {
 			StringBuilder str = new StringBuilder();
-			for(Character a: temp){
+			for (Character a : temp) {
 				str.append(a);
 			}
 			return str.toString();
 		}
+
 		@Override
 		public String toString() {
 			return "Automaton [pos=" + pos + ", index='" + index + "', state="
 					+ state.toUpperCase() + ", temp=\"" + temp + "\"]";
 		}
-		public void execEpsilon(){
+
+		public void execEpsilon() {
 			// volver a "initial", ejecucion epsilon
-			state= "initial";
+			state = "initial";
 		}
-		public void goBackAndReset(){
+
+		public void goBackAndReset() {
 			// volver el puntero 1 para atras
 			pos--;
-			//vaciar temporal
+			// vaciar temporal
 			temp.clear();
 			// restart pos inicial temporal
 			initialPos = -1;
 		}
-		
-		List<Token> tokens = Lists.newArrayList(); 
-		
+
+		List<Token> tokens = Lists.newArrayList();
+
 	}
+
+	public static void printMap(Map map) {
+		for (Object entObject : map.entrySet()) {
+			Entry entry = (Entry) entObject;
+			System.out.println(String.format(
+					"key (%s): '%s' value (%s): '%s' ", entry.getKey()
+							.getClass(), entry.getKey(), entry.getValue()
+							.getClass(), entry.getValue()));
+		}
+	}
+
 	public static void main(String[] args) {
 		init();
+		/**/
 		String input = "ab1 10. bb0b. 10.11 100101 a";
 		System.out.println("tokens:");
-		for(Token t : scan(input)){
+		for (Token t : scan(input)) {
 			System.out.println(t);
 		}
-		
-		System.out.println();
-		System.out.println(Yaml.dump(transitions));
+		/**/
 	}
+
 	private static List<Token> scan(String input) {
 		Automaton au = new Automaton();
-		
+
 		try {
 			while (au.pos <= input.length()) {
-				if(au.pos < input.length()){
+				if (au.pos < input.length()) {
 					au.index = input.charAt(au.pos++);
-				}else{
+				} else {
 					au.index = '$'; // end of file
 					au.pos++;
 				}
@@ -178,18 +118,36 @@ public class Main {
 				} else {
 					au.state = nextState;
 					if (semanticActions.containsKey(au.state)) {
-						Map<String,Object> action = semanticActions.get(au.state);
-						if(action.containsKey("save_value") && (Boolean)action.get("save_value")){
+						Map<String, Object> action = semanticActions
+								.get(au.state);
+						if (action.containsKey("save_value")
+								&& (Boolean) action.get("save_value")) {
+							System.out.println("guardo valor");
 							au.temp.add(au.index);
-							if(au.initialPos<0){
-								au.initialPos = au.pos-1;
+							if (au.initialPos < 0) {
+								au.initialPos = au.pos - 1;
 							}
-						}else{ // Estado final y por lo tanto epsilon a inicial
-							String tokenId = (String)action.get("token_id");
-							boolean reset = (Boolean)action.get("reset");
-							boolean haveValue = (Boolean)action.get("have_value");
-							au.tokens.add(new Token(tokenId, haveValue ? au.getTempString() : null));
-							if(reset){
+						} else { // Estado final y por lo tanto epsilon a
+									// inicial
+							String tokenId = (String) action.get("token_id");
+							boolean reset = (Boolean) action.get("reset");
+							boolean haveValue = (Boolean) action
+									.get("have_value");
+							Token token = new Token(tokenId,
+									haveValue ? au.getTempString() : null);
+							au.tokens.add(token);
+							if (haveValue) {
+								System.out.println(String.format(
+										"==> en pos %d hasta %d, (%s, %s)",
+										au.initialPos, au.pos - 2,
+										token.getCode(), token.getValue()));
+							} else {
+								System.out.println(String.format(
+										"==> en pos %d, (%s, null)",
+										au.pos - 1, token.getCode()));
+							}
+							if (reset) {
+								System.out.println("reset");
 								au.goBackAndReset();
 							}
 							au.execEpsilon();

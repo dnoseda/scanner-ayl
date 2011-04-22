0b0a27fd5e99d0606e5b526c2a8442a71780c0ab
dnoseda <damian.noseda@mercadolibre.com>
2011-04-22 16:19:45 -0300

    commenting sysouts

--- src/htmlscanner/Main.java
+++ src/htmlscanner/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	public static void printMap( Map map) {
 		for (Object entObject : map.entrySet()) {
 			Entry entry = (Entry) entObject;
-			System.out.println(String.format(
+			//System.out.println(String.format(
 					"key (%s): '%s' value (%s): '%s' ", entry.getKey()
 							.getClass(), entry.getKey(), entry.getValue()
 							.getClass(), entry.getValue()));
@@ -26,9 +26,9 @@ public class Main {
 		/**/
 		String input = "ab1 10. bb0b. 10.11 100101 a";
 		ScanResult result = au.scan(input);
-		System.out.println("tokens:");
+		//System.out.println("tokens:");
 		for (Token t : result.getTokens()) {
-			System.out.println(t);
+			//System.out.println(t);
 		}
 		/**/
 	}

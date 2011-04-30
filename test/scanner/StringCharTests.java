af3322010eae7a1db60d89c670c567cb9d888d83
dnoseda <damian.noseda@mercadolibre.com>
2011-04-30 11:40:28 -0300

    test for single string working greate

--- test/scanner/StringCharTests.java
+++ test/scanner/StringCharTests.java
@@ -5,10 +5,6 @@ import htmlscanner.Token;
 import java.util.Arrays;
 import java.util.List;
 
-import org.ho.yaml.Yaml;
-
-import com.google.common.collect.ImmutableMap;
-
 public class StringCharTests extends AutomatonTests {
 	public void testString() throws Exception {
 		ScanResult result = scan("\"astring 1.23.4 123\"");
@@ -16,7 +12,6 @@ public class StringCharTests extends AutomatonTests {
 		List<Token> expected = Arrays.asList(
 				new Token("STRING","astring 1.23.4 123")
 			);
-		System.out.println(Yaml.dump(ImmutableMap.of("\"","string")));
 		assertTrue(isEqual(expected,result.tokens));
 	}
 }

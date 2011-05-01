9eb58c8085c9d1d74f1548704e9f368d7ec3a6fc
dnoseda <damian.noseda@mercadolibre.com>
2011-05-01 20:10:17 -0300

    making work other tests

--- test/scanner/StringCharTests.java
+++ test/scanner/StringCharTests.java
@@ -8,5 +8,10 @@ public class StringCharTests extends AutomatonTests {
 	public void testStringAndNumber() throws Exception {
 		setLog(true);
 		assertTokens(token("INT","123").and("STRING","astring"),"123\"astring\"");
+		setLog(false);
+	}
+	
+	public void testStringAndIdent() throws Exception {
+		assertTokens(token("INT","123").and("SEP",null).and("ID","qwe12").and("STRING","otro"), "123 qwe12\"otro\"");
 	}
 }

adfe5e54bf612d6f7b4d2c4edfdcad0029720b91
dnoseda <damian.noseda@mercadolibre.com>
2011-05-03 02:51:04 -0300

    test for comments and refact for aliases; then library for use graphviz to graph the automaton

---
+++ test/scanner/CommentTests.java
@@ -0,0 +1,21 @@
+package scanner;
+
+import htmlscanner.TokenType;
+
+public class CommentTests extends AutomatonTests {
+	public void testCommentInLine() throws Exception {
+		String input = "// cualquier cosa\n" +
+				"i12341";
+		assertTokens(token(TokenType.ID,"i12341"), input);
+	}
+	
+	public void testLiteralAndComment() throws Exception {
+		String input = "9876.004 // cualquier cosa";
+		assertTokens(token(TokenType.REAL,"9876.004"), input);
+	}
+	
+	public void testMultilineComment() throws Exception {
+		assertTokens(token(TokenType.CHAR,"a").and(TokenType.STRING,"un string"),
+				"'a'/** lkjadhqlwkjqhelk q2p34534hqlkewh lkjrhasfdoiasdfasd\nj786123874 91b7978dafsdncsaodisudybvfisndc **/\"un string\"");
+	}
+}

0e09e5f49829a3b267fadaf8edd0032596c60551
hfloresleyes <hernan.floresleyes@mercadolibre.com>
2011-05-11 01:26:12 -0300

    Added commandline support to load conf and input.

--- src/htmlscanner/Main.java
+++ src/htmlscanner/Main.java
@@ -1,35 +1,44 @@
 package htmlscanner;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.io.FileUtils;
 
 import scanner.ScanResult;
-
 
 public class Main {
 
 	@SuppressWarnings("rawtypes")
-	public static void printMap( Map map) {
+	public static void printMap(Map map) {
 		for (Object entObject : map.entrySet()) {
 			Entry entry = (Entry) entObject;
-			//System.out.println(String.format(
-//					"key (%s): '%s' value (%s): '%s' ", entry.getKey()
-//							.getClass(), entry.getKey(), entry.getValue()
-//							.getClass(), entry.getValue()));
+			// System.out.println(String.format(
+			// "key (%s): '%s' value (%s): '%s' ", entry.getKey()
+			// .getClass(), entry.getKey(), entry.getValue()
+			// .getClass(), entry.getValue()));
 		}
 	}
 
 	public static void main(String[] args) {
 		Automaton au = new Automaton();
-		au.init("simpleconf.yaml");
+		au.init(args[0]);
 		/**/
-		String input = "ab1 10. bb0b. 10.11 100101 a";
-		ScanResult result = au.scan(input);
-		//System.out.println("tokens:");
-		for (Token t : result.getTokens()) {
-			//System.out.println(t);
+		String input;
+		try {
+			input = FileUtils.readFileToString(new File(args[1]));
+			ScanResult result = au.scan(input);
+			System.out.println("tokens:");
+			for (Token t : result.getTokens()) {
+				System.out.println(t);
+			}
+		} catch (IOException e) {
+			System.out.print(e.getMessage());
+			System.exit(1);
 		}
+		
 		/**/
 	}
 }
